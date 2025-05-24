e9bfef2125f46f2c028b65a675597ebaf5a4b4e6
kul3l3i <leiumarsayco95@gmail.com>
2025-05-25 02:03:51 +0800

    nive

--- cart.php
+++ cart.php
@@ -274,6 +274,7 @@ $selectedTotal = $selectedSubtotal + $shippingFee;
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Shopping Cart - HirayaFit</title>
+    <link rel="icon" href="images/logo.png">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="style/usershop.css">
    <style>
